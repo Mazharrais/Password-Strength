edbcd23c020fa4876a17efac0613614dc5ef96ce
Mazharrais <mazharmajo72@gmail.com>
2024-08-01 18:27:19 +0500

    add code

--- app.js
+++ app.js
@@ -12,4 +12,19 @@ pass.addEventListener('input', () =>{
     else{
         mesg.style.display = "none";
     }
+    if(pass.value.length < 4 ){
+        str.innerHTML = "weak";
+        pass.style.borderColor = "red";
+        mesg.style.color = "red";
+    }
+    else if(pass.value.length > 4 && pass.value.length < 8 ){
+        str.innerHTML = "medium";
+        pass.style.borderColor = "orange";
+        mesg.style.color = "orange";
+    }
+    else if(pass.value.length > 8){
+        str.innerHTML = "strong";
+        pass.style.borderColor = "green";
+        mesg.style.color = "green";
+    }
 })
