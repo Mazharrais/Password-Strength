73841ed5940438142db219b1db92538fbd8e3a2f
Mazharrais <mazharmajo72@gmail.com>
2024-08-01 16:52:05 +0500

    add code

---
+++ app.js
@@ -0,0 +1,9 @@
+var pass = document.getElementById('password');
+var mesg = document.getElementById('message');
+var str = document.getElementById('strength');
+
+
+
+function passwordStr(){
+    if
+}
